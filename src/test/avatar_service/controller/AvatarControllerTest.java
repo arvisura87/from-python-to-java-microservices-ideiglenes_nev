0eb818906a177414b6f0b5bb96ec17dd88b13646
lepkebocs <tkkadartamas@gmail.com>
2017-01-12 18:28:57 +0100

    test started

---
+++ src/test/avatar_service/controller/AvatarControllerTest.java
@@ -0,0 +1,16 @@
+package avatar_service.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by kadartamas on 2017.01.11..
+ */
+public class AvatarControllerTest {
+
+    @Test
+    public void getAvatarTest() throws Exception {
+    }
+
+}
---
+++ src/test/theme_service/controller/ThemeControllerTest.java
@@ -0,0 +1,20 @@
+package theme_service.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+/**
+ * Created by kadartamas on 2017.01.11..
+ */
+public class ThemeControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getTheme() throws Exception {
+    }
+}
